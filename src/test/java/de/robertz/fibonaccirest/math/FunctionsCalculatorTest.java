a6be63396cbe0fbdc22c966659be940bc108cc56
Robert Ziegon <rziegon@clausohm.de>
2022-05-01 17:36:10 +0200

    added Test

--- src/test/java/de/robertz/fibonaccirest/math/FunctionsCalculatorTest.java
+++ src/test/java/de/robertz/fibonaccirest/math/FunctionsCalculatorTest.java
@@ -8,6 +8,22 @@ class FunctionsCalculatorTest {
 
     private static final int MAX_FIB_NUMBER = 71;
 
+    private static final int[] FIB_NUMBERS = new int[]{1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144};
+
+    @Test
+    void testIterative() {
+        for (int i = 0; i < FIB_NUMBERS.length; i++) {
+            assertEquals(FunctionsCalculator.calculateFibonacciIterative(i + 1), FIB_NUMBERS[i]);
+        }
+    }
+
+    @Test
+    void testMoivreBinet() {
+        for (int i = 0; i < FIB_NUMBERS.length; i++) {
+            assertEquals(FunctionsCalculator.calculateFibonacciMoivreBinet(i + 1), FIB_NUMBERS[i]);
+        }
+    }
+
     @Test
     void calculateFibonacci() {
         for (int i = 1; i < MAX_FIB_NUMBER; i++) {
